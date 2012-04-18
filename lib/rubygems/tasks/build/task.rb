d60ea54aa05984f595076bdac738548aead74f71
Postmodern <postmodern.mod3@gmail.com>
2012-04-18 14:32:34 -0700

    Fixed a typo.

--- lib/rubygems/tasks/build/task.rb
+++ lib/rubygems/tasks/build/task.rb
@@ -33,7 +33,7 @@ module Gem
           gemspec_tasks "build:#{name}"
 
           desc "Builds all packages"
-          task :build, "build:#{name}"
+          task :build => "build:#{name}"
         end
 
         #
