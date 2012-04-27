463149cf4b66e630a5ff469891527062bd77027c
Postmodern <postmodern.mod3@gmail.com>
2012-04-26 17:35:17 -0700

    Fixed SCM::Tag specs.

--- spec/scm/tag_spec.rb
+++ spec/scm/tag_spec.rb
@@ -4,7 +4,7 @@ require 'rake_context'
 require 'rubygems/tasks/scm/tag'
 
 describe Gem::Tasks::SCM::Tag do
-  let(:version) { '1.2.3' }
+  let(:version) { '1.2.3'              }
 
   describe "#version_tag" do
     context "defaults" do
@@ -39,14 +39,15 @@ describe Gem::Tasks::SCM::Tag do
   end
 
   describe "#tag!" do
-    let(:name) { 'v1.2.3' }
+    let(:name)    { "v#{version}"        }
+    let(:message) { "Tagging #{name}"    }
 
     context "git" do
       include_context "rake"
 
       it "should run `git tag`" do
         subject.project.stub!(:scm).and_return(:git)
-        subject.should_receive(:run).with('git', 'tag', name)
+        subject.should_receive(:run).with('git', 'tag', '-m', message, name)
 
         subject.tag!(name)
       end
@@ -57,7 +58,7 @@ describe Gem::Tasks::SCM::Tag do
 
       it "should run `hg tag`" do
         subject.project.stub!(:scm).and_return(:hg)
-        subject.should_receive(:run).with('hg', 'tag', name)
+        subject.should_receive(:run).with('hg', 'tag', '-m', message, name)
 
         subject.tag!(name)
       end
