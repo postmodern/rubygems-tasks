40c65546f3e8381ee7ccc9d43f4ab3539ba188aa
Postmodern <postmodern.mod3@gmail.com>
2012-04-29 23:05:13 -0700

    Added specs for Sign::PGP.

---
+++ spec/sign/pgp_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'rake_context'
+
+require 'rubygems/tasks/sign/pgp'
+
+describe Gem::Tasks::Sign::PGP do
+  describe "#sign" do
+    include_context "rake"
+
+    let(:path) { File.join('pkg','foo-1.2.3.gem') }
+
+    it "should run `gpg --sign --detach-sign --armor ...`" do
+      subject.should_receive(:run).with(
+        'gpg', '--sign', '--detach-sign', '--armor', path
+      )
+
+      subject.sign(path)
+    end
+  end
+end
