901118dbbcd3d4cab9669776dc47d72444689069
Postmodern <postmodern.mod3@gmail.com>
2015-06-17 20:46:50 -0700

    Commited our test projects instead of auto-downloading them.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,25 @@
 require 'rspec'
 
-PROJECTS_DIR = File.join('data','projects')
+PROJECTS_DIR = File.expand_path('../projects',__FILE__)
 PROJECT_DIRS = lambda { |name| File.join(PROJECTS_DIR,name) }
-
-unless File.directory?(PROJECTS_DIR)
-  abort "Please run `rake data:projects` before running the specs!"
-end
 
 # clear the $RUBYCONSOLE env variable
 ENV['RUBYCONSOLE'] = nil
+
+RSpec.configure do |rspec|
+  rspec.before(:suite) do
+    Dir[File.join(PROJECTS_DIR,'*')].each do |dir|
+      Dir.chdir(dir) do
+        system 'git init'
+        system 'git add .'
+        system 'git commit -q -m "Initial commit"'
+      end
+    end
+  end
+
+  rspec.after(:suite) do
+    Dir[File.join(PROJECTS_DIR,'*')].each do |dir|
+      FileUtils.rm_rf(File.join(dir,'.git'))
+    end
+  end
+end
